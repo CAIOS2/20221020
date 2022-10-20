458e5254ff0febc720aaa1870c9f008a10110f3f
tadaspetrikas <67460225+tadaspetrikas@users.noreply.github.com>
2022-10-20 21:58:16 +0300

    Add sample code
    Adds sample code for using enum type in table view data source delegate

--- CIOS20221020/CIOS20221020/ViewController.swift
+++ CIOS20221020/CIOS20221020/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TableViewSections: CaseIterable, Int {
+    case oneSection
+    case anotherSection
+    case thirdSection
+    
+    var numberOfRows: Int {
+        return 3
+    }
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = TableViewSections(rawValue: section)
+        switch section {
+        case .oneSection:
+            return 10
+        default:
+            return 20
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return TableViewSections.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
