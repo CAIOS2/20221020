21fe7cb7c9129be3afd8cf7a309725fcf2a2a671
Rolandas-R <driveris@gmail.com>
2022-10-20 21:50:35 +0300

    Viewsai ir cele
    Viewsu ir celes deliojimas Storuboarde

---
+++ CIOS20221020/penktosSavaitesND/penktosSavaitesND/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  penktosSavaitesND
+//
+//  Created by reromac on 2022-10-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CIOS20221020/penktosSavaitesND/penktosSavaitesND/navigacijosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  navigacijosTableViewCell.swift
+//  penktosSavaitesND
+//
+//  Created by reromac on 2022-10-20.
+//
+
+import UIKit
+
+class navigacijosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
