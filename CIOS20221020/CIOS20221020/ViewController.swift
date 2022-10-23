db6be5608492b63069c54deaa34dc8300e95d7d3
DeividasZabulis <114018237+DeividasZabulis@users.noreply.github.com>
2022-10-23 18:39:11 +0300

    Added table view and prototype cell

--- CIOS20221020/CIOS20221020/ViewController.swift
+++ CIOS20221020/CIOS20221020/ViewController.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+//        recipeTableView.dataSource = self
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let restaurantNames = ["Cili pica", "Katpedele", "Kebabine", "Kibinine", "Surio namai"]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cele", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {return
+            "Restaurants"}
+        return "Recipes"
+    }
+    
+    
 }
+    
 
